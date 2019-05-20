33e0d9aff130a408e7933a6fa7b8c6d4ae7c1871
YANPUCHEN <YANPUCHEN@kingsoft.com>
2019-05-20 17:29:11 +0800

    Your branch is up to date with 'origin/master'.

--- webpack-children-router/src/app.vue
+++ webpack-children-router/src/app.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>这是我的vue组件</h1>
+        <h1>这是修改后的vue</h1>
         <router-link to="/account">Account</router-link>
         <router-link to="/goodslist">Goodslist</router-link>
         <router-view></router-view>
